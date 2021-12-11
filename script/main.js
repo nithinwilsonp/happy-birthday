8d608d420e76a02508048bdf65fd67f95ce21711
nithinwilsonp <71454694+nithinwilsonp@users.noreply.github.com>
2021-12-11 22:38:43 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -304,3 +304,30 @@ const animationTimeline = () => {
 
 // Run fetch and animation in sequence
 fetchData();
+
+
+var end = Date.now() + (15 * 1000);
+
+// go Buckeyes!
+var colors = ['#bb0000', '#ffffff'];
+
+(function frame() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors
+  });
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+}());
