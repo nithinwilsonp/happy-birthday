f4b0cf358fa42b9429ff620b24ed395326241b98
nithinwilsonp <71454694+nithinwilsonp@users.noreply.github.com>
2021-12-13 17:11:58 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -299,6 +299,26 @@ const animationTimeline = () => {
   const replyBtn = document.getElementById("replay");
   replyBtn.addEventListener("click", () => {
     tl.restart();
+    (function frame() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors
+  });
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors
+  });
+
+  if (Date.now() < end) {
+    requestAnimationFrame(frame);
+  }
+}());
   });
 };
 
